d7be739650a90372119bf5a8484c46050ef410de
Johnny Tai <bm414148@gmail.com>
2023-07-11 19:45:30 +0800

    set margin 0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.scss'
 import type { Metadata } from 'next'
 import React from 'react'
 import { Inter } from 'next/font/google'
+import Navbar from './components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
