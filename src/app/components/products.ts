152eafd68c65dabff02a499ae6b51982e89cedd9
Johnny Tai <bm414148@gmail.com>
2023-07-12 13:59:15 +0800

    add price

--- src/app/components/products.ts
+++ src/app/components/products.ts
@@ -3,19 +3,20 @@ export interface IProduct {
   category?: string;
   name: string;
   src: string;
+  price?: number;
 }
 
 
 export const products = [
-  {id:'1', category:"food", name:"Pizza", src:"/food/pizza.jpg"},
-  {id:'2', category:"food", name:"Burger", src:"/food/burger.jpg"},
-  {id:'3', category:"food", name:"Taco", src:"/food/taco.jpg"},
-  {id:'4', category:"drink", name:"Cola", src:"/drink/cola.jpg"},
-  {id:'5', category:"drink", name:"Pepsi", src:"/drink/pepsi.jpg"},
-  {id:'6', category:"drink", name:"Sprite", src:"/drink/sprite.jpg"},
-  {id:'7', category:"other", name:"Doritos", src:"/other/doritos.jpg"},
-  {id:'8', category:"other", name:"Chocolate", src:"/other/chocolate.jpg"},
-  {id:'9', category:"other", name:"Cake", src:"/other/cake.jpg"},
+  {id:'1', category:"food", name:"Pizza", src:"/food/pizza.jpg", price:199},
+  {id:'2', category:"food", name:"Burger", src:"/food/burger.jpg", price:99},
+  {id:'3', category:"food", name:"Taco", src:"/food/taco.jpg", price:129},
+  {id:'4', category:"drink", name:"Cola", src:"/drink/cola.jpg", price:35},
+  {id:'5', category:"drink", name:"Pepsi", src:"/drink/pepsi.jpg", price:29},
+  {id:'6', category:"drink", name:"Sprite", src:"/drink/sprite.jpg", price:30},
+  {id:'7', category:"other", name:"Doritos", src:"/other/doritos.jpg", price:35},
+  {id:'8', category:"other", name:"Chocolate", src:"/other/chocolate.jpg", price:20},
+  {id:'9', category:"other", name:"Cake", src:"/other/cake.jpg", price:40},
  
 ]
 
