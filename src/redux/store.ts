5a2673803eef30a57ad03acb9945d5d562be12da
Johnny Tai <bm414148@gmail.com>
2023-07-12 12:52:39 +0800

    add useAppSelector

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartSliceReducer from './features/cartSlice'
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
 export const store = configureStore({
   reducer: {cartSliceReducer},
 })
@@ -8,3 +9,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
