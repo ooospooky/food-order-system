b6e8404cdf4bbcd48ff0d9aeba21ed2f2f23e8ae
Johnny Tai <bm414148@gmail.com>
2023-07-12 15:15:16 +0800

    add getTotalAmount function

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -1,28 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
+import { products } from '@/app/components/products'
 interface IproductsList {
   [key: number]: number;
 }
 type InitialState = {
-  value : IproductsList;
+  value: IproductsList;
+  totalPrice: number;
 }
 
-let productsList:IproductsList ={}
-for(let i=1; i<=9; i++){
+let productsList: IproductsList = {}
+for (let i = 1; i <= 9; i++) {
   productsList[i] = 0;
 }
-let initialState:InitialState = {value:productsList}
+let initialState: InitialState = { value: productsList,totalPrice: 0}
 
 
 export const cartSlice = createSlice({
-  name:"cart",
+  name: "cart",
   initialState,
-  reducers:{
-    addToCart:(state,action:PayloadAction<any>)=>{
-      state.value[action.payload] +=1
-    }
+  reducers: {
+    addToCart: (state, action: PayloadAction<any>) => {
+      state.value[action.payload] += 1
+    },
+    getTotalCartAmount: (state, action: PayloadAction<any>) => {
+      let totalAmount = 0;
+      for (const key in state.value) {
+        if (state.value[key] !== 0) {
+          const product = products.find((p) => p.id === key);
+            totalAmount += product.price * state.value[key];
+        }
+      }
+      state.totalPrice = totalAmount;
+    },
   }
 })
 
-export const {addToCart} = cartSlice.actions;
+export const { addToCart,getTotalCartAmount } = cartSlice.actions;
 export default cartSlice.reducer;
