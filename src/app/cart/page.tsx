02d01b15108b92c712073608cb7031fa19d7af9d
Johnny Tai <bm414148@gmail.com>
2023-07-12 14:20:17 +0800

    add className for style & checkout div

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,42 @@
+"use client"
 import React from 'react'
+import './page.scss'
+import { addToCart } from '@/redux/features/cartSlice'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/redux/store'
+import { useAppSelector } from '@/redux/store'
 
-export default function Cart(){
+import { products } from '../components/products'
+import { CartItem } from '../components/cartItem'
+export default function Cart() {
+  // const dispatch = useDispatch<AppDispatch>()
+  const data = useAppSelector((state) => state.cartSliceReducer.value)
   return (
-    <div>page</div>
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {Object.keys(data).map((key: string) => {
+          if (data[key] !== 0) {
+            const product = products.find((item) => item.id === key);
+            if (product) {
+              return (
+                <CartItem data={product} key={key} />
+              );
+            }
+          }
+        })}
+      </div>
+      page
+      <div className="checkout">
+          <p> Subtotal: $</p>
+          <button > Continue Shopping </button>
+        
+            {" "}
+            Checkout{" "}
+        </div>
+    </div>
+
   )
 }
