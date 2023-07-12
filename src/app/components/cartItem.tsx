0e33d0df2808579cb6238ae4f67ae10bea334aa9
Johnny Tai <bm414148@gmail.com>
2023-07-12 15:31:12 +0800

    add countHandler with + - & amount

--- src/app/components/cartItem.tsx
+++ src/app/components/cartItem.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import { useAppSelector } from '@/redux/store'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/redux/store'
+import { removeFromCart,addToCart } from '@/redux/features/cartSlice'
+
 export const CartItem = (props: any) => {
   const { id, name, price, src } = props.data;
+  const data = useAppSelector((state) => state.cartSliceReducer.value)
+  const dispatch = useDispatch<AppDispatch>()
   return (
     <div className="cartItem" key={id}>
       <div className="cartImageWrap">
@@ -12,14 +19,11 @@ export const CartItem = (props: any) => {
           <b>{name}</b>
         </p>
         <p> Price: ${price}</p>
-        {/* <div className="countHandler">
-          <button onClick={() => removeFromCart(id)}> - </button>
-          <input
-            value={cartItems[id]}
-            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
-          />
-          <button onClick={() => addToCart(id)}> + </button>
-        </div> */}
+        <div className="countHandler">
+          <button onClick={() => dispatch(removeFromCart(id))}> - </button>
+          <p>Amount: {data[id]} </p>
+          <button onClick={() => dispatch(addToCart(id))}> + </button>
+        </div>
       </div>
     </div>
   );
