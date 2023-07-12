61e3cb335047b63728614ccb0efc4b21459ef6ed
Johnny Tai <bm414148@gmail.com>
2023-07-12 12:09:46 +0800

    cartSliceReducer done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import cartSliceReducer from './features/cartSlice'
 export const store = configureStore({
-  reducer: {},
+  reducer: {cartSliceReducer},
 })
