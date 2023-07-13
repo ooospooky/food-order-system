82fb1a01b277d8c6f4d7b6436d21474f7c115b29
Johnny Tai <bm414148@gmail.com>
2023-07-13 11:41:18 +0800

    add history btn in navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -8,18 +8,27 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 export default function Navbar() {
+  const handlePopupClick =()=>{
+    window.open('/history', '_blank', 'width=500,height=500');
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: '#292929' }}>
-        <Toolbar  sx={{ display:"flex",justifyContent:"space-between" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
           <Link href="/" >
             <Typography variant="h6" component="div">
               Home
             </Typography>
           </Link>
-          <Link href="/cart">
-            <Button color="inherit">Cart</Button>
-          </Link>
+          <div>
+          <Link href="/history">
+              <Button color="inherit">History</Button>
+            </Link>
+            {/* <Button onClick={handlePopupClick} color="inherit">History</Button> */}
+            <Link href="/cart">
+              <Button color="inherit">Cart</Button>
+            </Link>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
