60712a7eaf6ed01b395ac9ee97945906b3b3c54a
Johnny Tai <bm414148@gmail.com>
2023-07-13 16:16:17 +0800

    remove btn of cart, add MUI's badge with cartIcon, using redux state

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,8 +6,13 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useAppSelector } from '@/redux/store'
 
 export default function Navbar() {
+  const quantityArray = Object.values(useAppSelector((state) => state.cartSliceReducer.value))
+  const cartQuatitiy = quantityArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   const handlePopupClick = () => {
     window.open('/history', '_blank', 'width=500,height=500');
   }
@@ -22,8 +27,13 @@ export default function Navbar() {
           </Link>
           <div>
             <Button onClick={handlePopupClick} color="inherit">History</Button>
-            <Link href="/cart">
+            {/* <Link href="/cart">
               <Button color="inherit">Cart</Button>
+            </Link> */}
+            <Link href="/cart">
+              <Badge badgeContent={cartQuatitiy} color="primary" >
+                <ShoppingCartIcon sx={{ width: "25px", height: "25px" }} />
+              </Badge>
             </Link>
           </div>
         </Toolbar>
