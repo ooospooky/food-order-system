2bd94b0c704bb7d697ffebe62bc9b8e0a4114c9b
Johnny Tai <bm414148@gmail.com>
2023-07-19 18:02:38 +0800

    add get dispatch(getTotalCartAmount()) to handle puls & minus

--- src/app/components/cartItem.tsx
+++ src/app/components/cartItem.tsx
@@ -3,12 +3,21 @@ import Image from 'next/image'
 import { useAppSelector } from '@/redux/store'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@/redux/store'
-import { removeFromCart,addToCart } from '@/redux/features/cartSlice'
+import { removeFromCart, addToCart, getTotalCartAmount } from '@/redux/features/cartSlice'
 
 export const CartItem = (props: any) => {
   const { id, name, price, src } = props.data;
   const data = useAppSelector((state) => state.cartSliceReducer.value)
   const dispatch = useDispatch<AppDispatch>()
+
+  const handlePlus = () => {
+    dispatch(addToCart(id))
+    dispatch(getTotalCartAmount())
+  }
+  const handleMinus = () => {
+    dispatch(removeFromCart(id))
+    dispatch(getTotalCartAmount())
+  }
   return (
     <div className="cartItem" key={id}>
       <div className="cartImageWrap">
@@ -20,9 +29,9 @@ export const CartItem = (props: any) => {
         </p>
         <p> Price: ${price}</p>
         <div className="countHandler">
-          <button onClick={() => dispatch(removeFromCart(id))}> - </button>
+          <button onClick={handleMinus}> - </button>
           <p>Amount: {data[id]} </p>
-          <button onClick={() => dispatch(addToCart(id))}> + </button>
+          <button onClick={handlePlus}> + </button>
         </div>
       </div>
     </div>
